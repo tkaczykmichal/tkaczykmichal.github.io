c04501c581c000483bd2f9133f6bcb462f773853
Emkate <tkaczykmichal94@gmail.com>
2018-02-09 23:25:12 +0100

    navbar logo changing on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,31 @@
 $('a[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 600);
-                return false;
-            }
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 600);
+            return false;
         }
-    });
+    }
+});
+
+$(document).on('scroll', function(){
+    changeLogo();
+});
+
+function changeLogo() {
+    if($(document).scrollTop() >= $("#timeline").position().top){
+        $(".navbar-logo__minimal").removeClass("hidenav");
+        $(".navbar-logo").addClass("hidenav");
+    }
+    else{
+        $(".navbar-logo__minimal").addClass("hidenav");
+        $(".navbar-logo").removeClass("hidenav");
+    }
+}
+
+$(function(){
+    changeLogo();
+});
