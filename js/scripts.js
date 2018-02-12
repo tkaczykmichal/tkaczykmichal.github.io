bcdf1b30d267d92158cbfd9aebf268b4732cd16c
Emkate <tkaczykmichal94@gmail.com>
2018-02-12 21:15:50 +0100

    timeline changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-$('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$('a[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
         var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
             $('html, body').animate({
                 scrollTop: target.offset().top
@@ -11,16 +11,16 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
 });
 
-$(document).on('scroll', function(){
+$(document).on('scroll', function () {
     changeLogo();
 });
 
+
 function changeLogo() {
-    if($(document).scrollTop() >= $("#timeline").position().top){
+    if ($(document).scrollTop() >= $("#timeline").position().top) {
         $(".navbar-logo__minimal").removeClass("hidenav");
         $(".navbar-logo").addClass("hidenav");
-    }
-    else{
+    } else {
         $(".navbar-logo__minimal").addClass("hidenav");
         $(".navbar-logo").removeClass("hidenav");
     }
@@ -29,12 +29,12 @@ function changeLogo() {
 function clickOnYear() {
     $(".year-button").removeClass("year-button--selected");
     $(this).addClass("year-button--selected");
-    
+
     $(".year-content").removeClass("year-content--selected");
-    $('[data-content-year="'+ $(this).attr('data-year') +'"]').addClass("year-content--selected");
+    $('[data-content-year="' + $(this).attr('data-year') + '"]').addClass("year-content--selected");
 }
 
-$(function(){
+$(function () {
     changeLogo();
 
     $(".year-button").off('click').on('click', clickOnYear);
